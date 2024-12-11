ee6cb3e3496482915ff980c831c56994ace6d613
Risto "Dilaz" Viitanen <risto.viitanen@iki.fi>
2024-12-11 21:07:29 +0200

    Day 11: Optimize & clean the code a bit

--- src/bin/day11/solution.rs
+++ src/bin/day11/solution.rs
@@ -1,47 +1,52 @@
 #[path = "../../utils.rs"]
 pub mod utils;
-use std::collections::HashMap;
-
 pub use utils::Solution;
+
+use std::collections::HashMap;
 use miette::Result;
 pub struct Day11;
 
 const PART_1_BLINKS: u64 = 25;
 const PART_2_BLINKS: u64 = 75;
 
-trait NumLen {
+trait NumLenSplit {
     fn len(&self) -> u64;
+    fn is_even_length(&self) -> bool {
+        self.len() % 2 == 0
+    }
+    fn split(&self) -> (u64, u64);
 }
 
-impl NumLen for u64 {
+impl NumLenSplit for u64 {
     fn len(&self) -> u64 {
         self.ilog10() as u64 + 1
     }
+    fn split(&self) -> (u64, u64) {
+        let pow = 10u64.pow(self.len() as u32 / 2);
+        (*self / pow, *self % pow)
+    }
 }
 
-pub fn solve(stone: u64, n: u64, max_iterations: u64, cache: &mut HashMap<(u64, u64, u64), u64>) -> u64 {
-    if max_iterations == n {
+
+pub fn solve(stone: u64, n: u64, max_iterations: &u64, cache: &mut HashMap<(u64, u64), u64>) -> u64 {
+    if max_iterations == &n {
         return 1;
-    } else if let Some(cache_value ) = cache.get(&(stone, n, max_iterations)) {
+    } else if let Some(cache_value ) = cache.get(&(stone, n)) {
         return *cache_value;
     }
 
-    let result = 
-    if stone == 0 {
-        solve(1, n + 1, max_iterations, cache)
-    } else if stone.len() % 2 == 0 {
-        let mut new_stone = 0u64;
-        let mut stone = stone;
-        for i in 0..stone.len() / 2 {
-            new_stone += stone % 10 * 10u64.pow(i as u32);
-            stone /= 10;
-        }
-        solve(stone, n + 1, max_iterations, cache) + solve(new_stone, n + 1, max_iterations, cache)
-    } else {
-        solve(stone * 2024, n + 1, max_iterations, cache)
+    let result = match stone {
+        0 => solve(1, n + 1, max_iterations, cache),
+        num if num.is_even_length()  => {
+            let (left, right) = num.split();
+            solve(left, n + 1, max_iterations, cache) + solve(right, n + 1, max_iterations, cache)
+        },
+        _ => solve(stone * 2024, n + 1, max_iterations, cache),
     };
 
-    cache.insert((stone, n, max_iterations), result);
+    // Save result to the cache
+    cache.insert((stone, n), result);
+
     result
 }
 
@@ -54,7 +59,7 @@ impl Solution<u64> for Day11 {
             .collect::<Vec<u64>>();
         let mut cache = HashMap::new();
 
-        Ok(stones.into_iter().map(|stone| solve(stone, 0, PART_1_BLINKS, &mut cache)).sum())
+        Ok(stones.into_iter().map(|stone| solve(stone, 0, &PART_1_BLINKS, &mut cache)).sum())
     }
     
     #[tracing::instrument]
@@ -65,7 +70,7 @@ impl Solution<u64> for Day11 {
             .collect::<Vec<u64>>();
         let mut cache = HashMap::new();
 
-        Ok(stones.into_iter().map(|stone| solve(stone, 0, PART_2_BLINKS, &mut cache)).sum())
+        Ok(stones.into_iter().map(|stone| solve(stone, 0, &PART_2_BLINKS, &mut cache)).sum())
     }
 }
 
